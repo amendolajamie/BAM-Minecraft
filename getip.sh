dde2eb7b8e4548f242e20cb917623811ba959330
James Amendolagine <jamie.amendolagine@gmail.com>
2023-12-15 12:13:06 -0800

    First commit

---
+++ getip.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+touch test.html
+
+
+var=$(dig +short myip.opendns.com @resolver1.opendns.com)
+
+
+cat > server.html << EOF
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>BAM Minecraft Server</title>
+  </head>
+  <body>
+    <h1>IP: $var </h1>
+    <h1>port: 19132</h1>
+  </body>
+</html>
+
+EOF
