e8a1a86ca865f8a376deb026e9a67a8ea7d4f600
James Amendolagine <jamie.amendolagine@gmail.com>
2023-12-16 05:00:01 -0800

    Sat Dec 16 05:00:01 AM PST 2023

--- getip.sh
+++ getip.sh
@@ -7,15 +7,8 @@ var=$(dig +short myip.opendns.com @resolver1.opendns.com)
 
 cat > server.html << EOF
 
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>BAM Minecraft Server</title>
-  </head>
-  <body>
-    <h1>IP: $var </h1>
-    <h1>port: 19132</h1>
-  </body>
-</html>
+BAM Minecraft Server $(date)
+IP: $var
+port: 19132
 
 EOF
